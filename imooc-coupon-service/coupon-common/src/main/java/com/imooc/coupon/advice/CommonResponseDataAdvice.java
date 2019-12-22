c911fc424bc71df94547f2666f477d93d1f56ce2
zhangbin <wx_ce3f3b5ad18e471b8e599dc60b4e18dc@git.code.tencent.com>
2019-12-22 19:48:16 +0400

    统一响应

--- imooc-coupon-service/coupon-common/src/main/java/com/imooc/coupon/advice/CommonResponseDataAdvice.java
+++ imooc-coupon-service/coupon-common/src/main/java/com/imooc/coupon/advice/CommonResponseDataAdvice.java
@@ -6,12 +6,14 @@ import org.springframework.core.MethodParameter;
 import org.springframework.http.MediaType;
 import org.springframework.http.server.ServerHttpRequest;
 import org.springframework.http.server.ServerHttpResponse;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyAdvice;
 
 /**
  * 统一响应
  */
 @SuppressWarnings("all")
+@RestControllerAdvice
 public class CommonResponseDataAdvice implements ResponseBodyAdvice {
     /**
      * 判断是否对相应进行处理
